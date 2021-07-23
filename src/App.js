4e63901d824889587250e72c65d453e34271acde
Marat Kotik <mkotik@Marats-MBP.fios-router.home>
2021-07-23 12:54:15 -0400

    created header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
+import { Route, Link } from "react-router-dom";
+import Homepage from "./Homepage/Homepage";
 
 const App = () => {
   return (
     <>
+      <Route exact path="/">
+        <Homepage />
+      </Route>
       <h1>Lambda Eats</h1>
       <p>You can remove this code and create your own header</p>
     </>
---
+++ src/Header.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Header.css";
+import { Link } from "react-router-dom";
+
+export default function Header() {
+  return (
+    <div className="headerWrap">
+      <h1 className="title">Lamba Eats</h1>
+      <nav>
+        <div className="homeBtnWrap">
+          <Link className="homeBtn" to="/">
+            Home
+          </Link>
+        </div>
+        <div className="helpBtnWrap">
+          <Link className="helpBtn" to="/">
+            Help
+          </Link>
+        </div>
+      </nav>
+    </div>
+  );
+}
